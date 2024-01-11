b3d97a821832c109a298683dddbf365965113cbd
Dennis H. <71262926+mnkyjs@users.noreply.github.com>
2024-01-11 22:44:08 +0100

    Add skeleton loading screen and remove redundant code. Prepare default app example

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { MapComponent } from './leaflet/feature/map/map.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MapComponent],
+  imports: [MapComponent],
   selector: 'px-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
