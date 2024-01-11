1fbc17ae7082c3f503ac0fb90910290b17687e65
Dennis H. <71262926+mnkyjs@users.noreply.github.com>
2024-01-11 22:47:25 +0100

    Update AppComponent to dynamically load MapComponent. This reduces the init bundle size

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChildren,
+  AfterViewInit,
+  ViewContainerRef,
+  QueryList,
+} from '@angular/core';
 import { MapComponent } from './leaflet/feature/map/map.component';
 
 @Component({
@@ -8,4 +14,16 @@ import { MapComponent } from './leaflet/feature/map/map.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  @ViewChildren('map', { read: ViewContainerRef })
+  vcr!: QueryList<ViewContainerRef>;
+
+  async ngAfterViewInit() {
+    const dfc = await import('./leaflet/feature/map/map.component').then(
+      (c) => c.MapComponent
+    );
+    this.vcr.map((viewContainerRef: ViewContainerRef) => {
+      viewContainerRef.createComponent(dfc);
+    });
+  }
+}
