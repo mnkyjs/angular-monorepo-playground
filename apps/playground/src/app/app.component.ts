f450b947eed3c75c109f2b491f24e399598ec5c6
Dennis H. <71262926+mnkyjs@users.noreply.github.com>
2024-01-13 14:12:38 +0100

    introduce defer block

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  ViewChildren,
-  AfterViewInit,
-  ViewContainerRef,
-  QueryList,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { MapComponent } from './leaflet/feature/map/map.component';
 
 @Component({
@@ -14,16 +8,4 @@ import { MapComponent } from './leaflet/feature/map/map.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChildren('map', { read: ViewContainerRef })
-  vcr!: QueryList<ViewContainerRef>;
-
-  async ngAfterViewInit() {
-    const dfc = await import('./leaflet/feature/map/map.component').then(
-      (c) => c.MapComponent
-    );
-    this.vcr.map((viewContainerRef: ViewContainerRef) => {
-      viewContainerRef.createComponent(dfc);
-    });
-  }
-}
+export class AppComponent {}
