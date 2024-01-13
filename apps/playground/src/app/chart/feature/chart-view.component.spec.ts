0bcb227a40005857af54953b132815b397595b94
Dennis H. <71262926+mnkyjs@users.noreply.github.com>
2024-01-13 14:00:08 +0100

    Add testing example for defer blocks

--- apps/playground/src/app/chart/feature/chart-view.component.spec.ts
+++ apps/playground/src/app/chart/feature/chart-view.component.spec.ts
@@ -1,21 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  DeferBlockState,
+} from '@angular/core/testing';
 import { ChartViewComponent } from './chart-view.component';
 
 describe('ChartViewComponent', () => {
-  let component: ChartViewComponent;
   let fixture: ComponentFixture<ChartViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ChartViewComponent],
-    }).compileComponents();
-
+  beforeEach( () => {
     fixture = TestBed.createComponent(ChartViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should test and query defer blocks', async () => {
+    const deferBlockFixture = (await fixture.getDeferBlocks())[0];
+
+    expect(fixture.nativeElement.innerHTML).toContain(
+      'A secret pokemon is loading...'
+    );
+
+    await deferBlockFixture.render(DeferBlockState.Loading);
+    expect(fixture.nativeElement.innerHTML).toContain(
+      '<div class="px-skeleton"></div>'
+    );
+
+    await deferBlockFixture.render(DeferBlockState.Complete);
+    expect(fixture.nativeElement.innerHTML).toContain('<px-chart>');
+  });
+
+  it('should test and query error defer block', async () => {
+    const deferBlockFixture = (await fixture.getDeferBlocks())[0];
+
+    await deferBlockFixture.render(DeferBlockState.Error);
+    expect(fixture.nativeElement.innerHTML).toContain(
+      '<p>Something went wrong</p>'
+    );
   });
 });
